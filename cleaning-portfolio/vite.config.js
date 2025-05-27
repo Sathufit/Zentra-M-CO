b36b6ff7c49f99cfa9e68dcfcbdb63f96b632b04
sathushnanayakkara <sathufit@gmail.com>
2025-05-27 07:49:41 +0530

    fix: added base path in vite config

--- cleaning-portfolio/vite.config.js
+++ cleaning-portfolio/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ ensures assets load correctly on Netlify
+  base: './', // ✅ needed for Netlify routing
 })
